4458c2c2411f93f6ddabd52b3edf5f507964bec2
Maaz Zindani <maaz@finosys.com>
2023-06-06 13:25:54 +0500

    njwnjnwdjdwjwdn

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:services_app/views/home/homepage.dart';
@@ -14,8 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      title: 'CarServicesLtd',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      builder: BotToastInit(),
+      navigatorObservers: [BotToastNavigatorObserver()],
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/network/Api.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:bot_toast/bot_toast.dart';
+import 'package:get_storage/get_storage.dart';
+
+const String apiKey = 'AIzaSyAKq-DUYZBLcHeCowr6AtGLm8lBssOmLew';
+
+class Api {
+  var sp = GetStorage();
+}
